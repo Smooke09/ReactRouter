206efa9728257359e41de46f9ac44502fdccc776
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-09 12:34:30 -0300

    simple navigation

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,10 +1,15 @@
 import "./Content.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom"; // Importe esses arquivos
+import About from "../../views/examples/About";
+import Home from "../../views/examples/Home";
 
 const Content = (props) => (
   <main className="Content">
-    <h1>Componente Content</h1>
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   </main>
 );
 
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import React from "react";
 
-import { BrowserRouter } from "react-router-dom"; // essa importação mudou
+import { BrowserRouter as Router } from "react-router-dom"; // essa importação mudou
 
 import Menu from "../components/layout/Menu";
 import Content from "../components/layout/Content";
 
 const App = (props) => (
   <div className="App">
-    <BrowserRouter>
+    <Router>
       <Menu />
       <Content />
-    </BrowserRouter>
+    </Router>
   </div>
 );
 
