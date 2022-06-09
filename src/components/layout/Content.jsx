8b9e4bf64afc9f9d66ac98a7145ab197cd759f4b
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-09 12:17:35 -0300

    React Router Dom V6

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,6 +1,11 @@
 import "./Content.css";
 import React from "react";
+import { Routes, Route } from "react-router-dom"; // Importe esses arquivos
 
-const Content = (props) => <main className="Content">Componente Content</main>;
+const Content = (props) => (
+  <main className="Content">
+    <h1>Componente Content</h1>
+  </main>
+);
 
 export default Content;
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,6 +1,18 @@
 import "./App.css";
 import React from "react";
 
-const App = (props) => <div className="App">Componente App</div>;
+import { BrowserRouter } from "react-router-dom"; // essa importação mudou
+
+import Menu from "../components/layout/Menu";
+import Content from "../components/layout/Content";
+
+const App = (props) => (
+  <div className="App">
+    <BrowserRouter>
+      <Menu />
+      <Content />
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
