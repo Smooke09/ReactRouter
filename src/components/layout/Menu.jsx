05514828f3c4e710dd1df1530dcf19dbd43e61ea
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-10 08:10:46 -0300

    navigation with parameter

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -1,6 +1,5 @@
 import "./Menu.css";
 import React from "react";
-
 import { Link } from "react-router-dom";
 
 const Menu = (props) => (
@@ -9,6 +8,12 @@ const Menu = (props) => (
       <ul>
         <li>
           <Link to="/">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/param/123">Param #01</Link>
+        </li>
+        <li>
+          <Link to="/param/legal">Param #02</Link>
         </li>
         <li>
           <Link to="/about">Sobre</Link>
